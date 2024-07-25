64308e8689d3b0b9df35a6c5c255001031b68c89
Yusuf <yusufadeyinka55@gmail.com>
2024-07-25 09:21:42 +0100

    lc

--- src/components/Designs.jsx
+++ src/components/Designs.jsx
@@ -3,7 +3,7 @@ import '../components/css/Designs.css'
 import cat1 from '../assets/cat1.jpg';
 import cat2 from '../assets/cat2.jpg';
 import cat3 from '../assets/cat3.jpg';
-
+import cat4 from '../assets/cat4.jpg';
 function Designs() {
   return (
     <div className='designs' >
@@ -11,6 +11,7 @@ function Designs() {
           <div className="d-box"> <img className='cat-img' src={cat1} alt="" /> <div className="cat-name"> New Collection </div> </div>
           <div className="d-box"> <img className='cat-img' src={cat2} alt="" /> <div className="cat-name"> Exclusive Designs </div> </div>
           <div className="d-box"> <img className='cat-img' src={cat3} alt="" /> <div className="cat-name"> Luxurious Fabrics </div> </div>
+          <div className="d-box"> <img className='cat-img' src={cat4} alt="" /> <div className="cat-name"> Luxurious Fabrics </div> </div>
         </div>
     </div>
   )
